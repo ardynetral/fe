6e18ae91d401ec22f6d9063d20eb1f873fd489f4
ardy <syukrunyuniardi@gmail.com>
2021-11-24 07:43:18 +0700

    config db

--- modules/Did/Controllers/Did.php
+++ modules/Did/Controllers/Did.php
@@ -253,4 +253,106 @@ class Did extends \CodeIgniter\Controller
 		return $option; 
 		die();
 	}		
+
+	public function reportPdf() {
+		$db = \Config\Database::connect();
+		$sql_body = "
+		select 
+		cp.cpopr as principal,
+		month(cp.cpitgl) as dinfo_month,
+		day(cp.cpitgl) as dinfo_daily,
+		count(cp.crno) as total
+		from container_process cp
+		where year(cp.cpitgl)=year(now())
+		group by cp.cpopr, month(cp.cpitgl), day(cp.cpitgl)
+		";
+		
+		$data_body = $db->query($sql_body)->getResultArray();		
+
+		$mpdf = new \Mpdf\Mpdf();
+		$html = '';
+
+		$html .= '
+		<html>
+			<head>
+				<title>Report Depo Info Daily</title>
+
+				<style>
+					body{font-family: Arial;font-size:12px;}
+					.page-header{display:block;border-bottom:2px solid #aaa;padding:0;min-height:30px;margin-bottom:30px;}
+					.head-left{float:left;width:200px;padding:0px;}
+					.head-right{float:left;padding:0px;margin-left:200px;text-align: right;}
+
+					.tbl_head_prain, .tbl_det_prain{border-spacing: 0;border-collapse: collapse;}
+					.tbl_head_prain td{border-collapse: collapse;}
+					.t-right{text-align:right;}
+					.t-left{text-align:left;}
+
+					.tbl_det_prain th,.tbl_det_prain td {
+						border:1px solid #666666!important;
+						border-spacing: 0;
+						border-collapse: collapse;
+						padding:5px;
+
+					}
+					.line-space{border-bottom:1px solid #dddddd;margin:30px 0;}
+				</style>
+			</head>
+		';
+
+		$html .= '<body>
+			<div class="page-header">
+				<div class="head-left">
+					<h4>PT. CONTINDO RAYA</h4>
+				</div>
+				<div class="head-right">
+					<p>PADANG, '.date('d/m/Y').'</p>		
+				</div>
+			</div>
+		';
+		$html .='
+			<table class="tbl_head_prain" width="100%">
+				<tbody>
+					
+				</tbody>
+			</table>
+		';
+
+		$html .='
+			<h4>Repor Depo Info Daily</h4>
+			<table class="tbl_det_prain">
+				<thead>
+					<tr>
+						<th>NO</th>
+						<th>Principal</th>
+						<th>Info Daily</th>
+						<th>Info Month</th>
+						<th>Total</th>
+					</tr>
+				</thead>
+				<tbody>';
+				$no=1;
+				foreach($data_body as $row){
+					$html .='
+					<tr>
+						<td>'.$no.'</td>
+						<td>'.$row['principal'].'</td>
+						<td>'.$row['dinfo_month'].'</td>
+						<td>'.$row['dinfo_daily'].'</td>
+						<td>'.$row['total'].'</td>
+					</tr>';
+
+					$no++;
+				}
+		$html .='
+				</tbody>
+			</table>
+
+		</body>
+		</html>
+		';
+		$mpdf->WriteHTML($html);
+		$mpdf->Output();
+		die();				
+	}
 }
--- modules/Did/Views/js.php
+++ modules/Did/Views/js.php
@@ -121,7 +121,10 @@ $(document).ready(function() {
 			}
 		});		
 	}
-
-
+	// print PDF
+	$("#printPdf").on("click", function(e) {
+		window.open("<?php echo site_url('did/reportPdf'); ?>", '_blank', 'height=600,width=900,toolbar=no,directories=no,status=no, menubar=no,scrollbars=no,resizable=no ,modal=yes');		
+		e.preventDefault();
+	});
 });
 </script>

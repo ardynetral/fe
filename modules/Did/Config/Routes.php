e7beba8f1c171688568d19e93ca949cc5dc0cfa8
ardy <syukrunyuniardi@gmail.com>
2021-11-24 11:19:17 +0700

    routes did

--- modules/Did/Config/Routes.php
+++ modules/Did/Config/Routes.php
@@ -8,4 +8,5 @@ $routes->add('/did/edit/(:alphanum)', '\Modules\Did\Controllers\Did::edit/$1', [
 $routes->post('/did/edit/(:alphanum)', '\Modules\Did\Controllers\Did::edit/$1', ['filter' => 'login']);
 $routes->add('/did/delete/(:alphanum)', '\Modules\Did\Controllers\Did::delete/$1', ['filter' => 'login']);
 $routes->add('/did/ajax_country', '\Modules\Did\Controllers\Did::ajax_country', ['filter' => 'login']);
+$routes->add('/did/reportPdf', '\Modules\Did\Controllers\Did::reportPdf', ['filter' => 'login']);
 // $routes->add('/dailymovementout/ajax_customer', '\Modules\Principal\Controllers\Principal::ajax_country', ['filter' => 'login']);

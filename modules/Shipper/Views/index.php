1b38b833b44636c2439f6c7e2795b01da85a4b8c
yomz <yhomanez@users.noreply.github.com>
2021-11-30 16:50:54 +0700

    Module Shipper, Trucker,Forwading

---
+++ modules/Shipper/Views/index.php
@@ -0,0 +1,61 @@
+<?= $this->extend('smartdepo/dashboard/template') ?>
+
+<?= $this->section('content') ?>
+
+<div class="content">
+	<div class="main-header">
+		<h2><?=$page_title?></h2>
+		<em><?=$page_title?> page</em>
+	</div>
+
+	<?php if(session()->getFlashdata('sukses')):?>
+	<div class="alert alert-success alert-dismissable">
+		<a href="" class="close">×</a>
+		<strong><?=session()->getFlashdata('sukses');?></strong>
+	</div>
+	<?php endif; ?>
+
+	<div class="main-content">
+
+		<div class="widget widget-table">
+			<div class="widget-header">
+				<h3><i class="fa fa-table"></i> <?=$page_title?></h3></div>
+			<div class="widget-content">
+				<div class="row">
+					<div class="col-md-12">
+						<a href="<?=site_url('shipper/add');?>"class="btn btn-primary"><i class="fa fa-plus-square"></i>&nbsp;Add New</a>
+					</div>
+				</div><br>	
+
+				<div class="row">
+					<div class="col-md-12">
+
+						<br>
+						<table id="ctTable" class="table table-hover table-bordered" style="width:100%;">
+							<thead>
+								<tr>
+									<th>No.</th>
+									<th>Customer Code</th>
+									<th>Customer Name</th>
+									<th></th>
+								</tr>
+							</thead>
+							
+						</table>
+					</div>
+				</div>
+
+			</div>		
+		</div>		
+		
+	</div>
+</div>
+
+<?= $this->endSection();?>
+
+<!-- JS -->
+<?= $this->Section('script_js');?>
+
+	<?= $this->include('\Modules\Shipper\Views\js'); ?>	
+	
+<?= $this->endSection();?>
---
+++ modules/Trucker/Views/index_data.php
@@ -0,0 +1,11 @@
+<table id="ctTable" class="table table-hover table-bordered" style="width:100%;">
+	<thead>
+		<tr>
+			<th>No.</th>
+			<th>Customer Code</th>
+			<th>Customer Name</th>
+			<th></th>
+		</tr>
+	</thead>
+	
+</table>

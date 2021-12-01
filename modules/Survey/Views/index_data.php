c485a123fd8c47e5beba6abc95a03fe905a90161
ardy <syukrunyuniardi@gmail.com>
2021-12-01 22:25:24 +0700

    paging1

--- modules/Survey/Views/index_data.php
+++ modules/Survey/Views/index_data.php
@@ -1,7 +1,9 @@
-<?php if($data ==''): ?>
-	<p class="alert alert-warning"> Data not found.</p>
-<?php else : ?>
 
+<style type="text/css">
+	.btn-tbl{
+		margin-right: 5px;
+	}
+</style>
 <div class="row">
 	<div class="col-md-12">
 
@@ -19,41 +21,10 @@
 				</tr>
 			</thead>
 			
-			<tbody>
-				<?php $i=1; 
-				foreach($data as $row): ?>
-					<tr>
-						<td><?=$nomor++;?></td>
-						<td><?=$row['CRNO']?></td>
-						<td><?=$row['PRCODE']?></td>
-						<td><?=$row['CPITGL']?></td>
-						<td><?=$row['SVSURDAT']?></td>
-						<td><?=$row['SVCOND']?></td>
-						<td>
-							<a href="#" id="" class="btn btn-xs btn-primary" data-praid="">view</a>
-
-							<?php if(has_edit==true): ?>
-							<a href="#" id="editPraIn" class="btn btn-xs btn-success">edit</a>
-							<?php endif; ?>
 							
-							<?php if(has_print==true): ?>
-							<a href="#" class="btn btn-xs btn-info" data-praid="">print</a>
-							<?php endif; ?>
-
-							<?php if(has_delete==true): ?>
-							<a href="#" id="deletePraIn" class="btn btn-xs btn-danger">delete</a>
-							<?php endif; ?>
-
-						</td>
-					</tr>
-				<?php $i++; endforeach; ?>
-			</tbody>
 		</table>
 
 	</div>
-	<div class="col-sm-12">
-		<p><?=$pager->links('survey');?></p>
-	</div>			
+
 </div>
 
-<?php endif; ?>	

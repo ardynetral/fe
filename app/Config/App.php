c3fb93d32ba1168ff428b8902218fba397b64782
bertorington <bertorington@gmail.com>
2021-12-05 08:28:29 +0700

    app

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost/fe';
+	public $baseURL = 'http://202.157.185.83/smartdepo';
 
 	/**
 	 * --------------------------------------------------------------------------

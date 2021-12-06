6386d4b70dfd0b75ca686a0cf946389cb31316ba
yomz <yhomanez@users.noreply.github.com>
2021-12-06 11:28:03 +0700

    bug fix Contract

--- modules/Contract/Controllers/Contract.php
+++ modules/Contract/Controllers/Contract.php
@@ -32,16 +32,34 @@ class Contract extends \CodeIgniter\Controller
 
 	public function list_data() 
 	{
-		$offset = ($this->request->getPost('start')!= 0)?$this->request->getPost('start'):0;
-        $limit = ($this->request->getPost('rows') !="")? $this->request->getPost('rows'):10;		
+		$search = ($this->request->getPost('search[value]') != "")?$this->request->getPost('search[value]'):"";
+        $offset = ($this->request->getPost('start')!= 0)?$this->request->getPost('start'):0;
+        $limit = ($this->request->getPost('rows') !="")? $this->request->getPost('rows'):10;
+        $sort_column = $this->request->getPost('order[0][column]');	
+        $sort_type = $this->request->getPost('order[0][dir]');		
+        $orderColumn = '';
+        if ($sort_column == '0') {
+        	$orderColumn = '';
+        } elseif ($sort_column == '1') {
+        	$orderColumn = 'cono';
+        } elseif ($sort_column == '2') {
+        	$orderColumn = 'prcode';
+        } elseif ($sort_column == '3') {
+        	$orderColumn = 'codate';
+        } elseif ($sort_column == '4') {
+        	$orderColumn = 'coexpdate';
+        }				
 		$response = $this->client->request('GET','contracts/list',[
 			'headers' => [
 				'Accept' => 'application/json',
 				'Authorization' => session()->get('login_token')
 			],
-			'query'=>[
-				'start'=>$offset,
-				'rows'=>$limit
+			'json' => [
+				'start' => (int)$offset,
+				'rows'	=> (int)$limit,
+				'search'=> (string)$search,
+				'orderColumn' => (string)$orderColumn,
+				'orderType' => (string)$sort_type
 			]
 		]);
 

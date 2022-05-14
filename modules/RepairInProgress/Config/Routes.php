ffdadb81c5815895c67bbcb3c82b9cef707c76b3
yomz <yhomanez@gmail.com>
2022-05-14 12:39:10 +0700

    update MNR/Repair in Progress

--- modules/RepairInProgress/Config/Routes.php
+++ modules/RepairInProgress/Config/Routes.php
@@ -2,7 +2,17 @@
 
 $routes->add('/rip', '\Modules\RepairInProgress\Controllers\RepairInProgress::index', ['filter' => 'login']);
 $routes->add('/rip/add', '\Modules\RepairInProgress\Controllers\RepairInProgress::add', ['filter' => 'login']);
-// $routes->add('/prain/view/(:alphanum)', '\Modules\PraIn\Controllers\PraIn::view/$1', ['filter' => 'login']);
-// $routes->add('/prain/add', '\Modules\PraIn\Controllers\PraIn::add', ['filter' => 'login']);
-// $routes->add('/prain/edit/(:alphanum)', '\Modules\PraIn\Controllers\PraIn::edit/$1', ['filter' => 'login']);
-// $routes->add('/prain/delete/(:alphanum)', '\Modules\PraIn\Controllers\PraIn::delete/$1', ['filter' => 'login']);
+$routes->add('/rip/edit/(:alphanum)', '\Modules\RepairInProgress\Controllers\RepairInProgress::edit/$1', ['filter' => 'login']);
+$routes->add('/rip/list_data', '\Modules\RepairInProgress\Controllers\RepairInProgress::list_data', ['filter' => 'login']);
+$routes->add('/rip/getDataEstimasi', '\Modules\RepairInProgress\Controllers\RepairInProgress::getDataEstimasi', ['filter' => 'login']);
+$routes->add('/rip/getOneEstimasi/(:alphanum)', '\Modules\RepairInProgress\Controllers\RepairInProgress::getOneEsftimasi/$1', ['filter' => 'login']);
+// DETAIL
+$routes->add('/rip/add_detail', '\Modules\RepairInProgress\Controllers\RepairInProgress::add_detail', ['filter' => 'login']);
+$routes->add('/rip/save_detail', '\Modules\RepairInProgress\Controllers\RepairInProgress::save_detail', ['filter' => 'login']);
+$routes->add('/rip/update_detail', '\Modules\RepairInProgress\Controllers\RepairInProgress::update_detail', ['filter' => 'login']);
+$routes->add('/rip/getFileList/(:alphanum)/(:alphanum)', '\Modules\RepairInProgress\Controllers\RepairInProgress::getFileList/$1/$2', ['filter' => 'login']);
+$routes->add('/rip/delete_detail', '\Modules\RepairInProgress\Controllers\RepairInProgress::delete_detail', ['filter' => 'login']);
+$routes->add('/rip/delete_detail_edit', '\Modules\RepairInProgress\Controllers\RepairInProgress::delete_detail_edit', ['filter' => 'login']);
+$routes->add('/rip/print/(:alphanum)/(:alphanum)', '\Modules\RepairInProgress\Controllers\RepairInProgress::print/$1/$2', ['filter' => 'login']);
+$routes->add('/rip/calculateTotalCost', '\Modules\RepairInProgress\Controllers\RepairInProgress::calculateTotalCost', ['filter' => 'login']);
+
--- modules/RepairInProgress/Views/index_data.php
+++ modules/RepairInProgress/Views/index_data.php
@@ -1,7 +1,9 @@
-<?php if($data ==''): ?>
-	<p class="alert alert-warning"> Data not found.</p>
-<?php else : ?>
 
+<style type="text/css">
+	.btn-tbl{
+		margin-right: 5px;
+	}
+</style>
 <div class="row">
 	<div class="col-md-12">
 
@@ -12,53 +14,18 @@
 					<th>No.</th>
 					<th>Container No</th>
 					<th>Principal</th>
-					<th>Gate In</th>
+					<th>Gate In Date</th>
+					<th>Survey Date</th>
 					<th>Condition</th>
-					<th>WO No.</th>
-					<th>WO Date</th>
-					<th>Completed</th>
-					<th>Last Condition</th>
+					<th>EOR No.</th>
+					<th>Version</th>
 					<th></th>
 				</tr>
 			</thead>
+			<tbody></tbody>
 			
-			<tbody>
-				<?php $i=1; foreach($data as $row): ?>
-					<tr>
-						<td><?=$nomor++;?></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td>
-							<a href="#" id="" class="btn btn-xs btn-primary" data-praid="">view</a>
-
-							<?php if(has_edit==true): ?>
-							<a href="#" id="editPraIn" class="btn btn-xs btn-success">edit</a>
-							<?php endif; ?>
 							
-							<?php if(has_print==true): ?>
-							<a href="#" class="btn btn-xs btn-info" data-praid="">print</a>
-							<?php endif; ?>
-
-							<?php if(has_delete==true): ?>
-							<a href="#" id="deletePraIn" class="btn btn-xs btn-danger">delete</a>
-							<?php endif; ?>
-
-						</td>
-					</tr>
-				<?php $i++; endforeach; ?>
-			</tbody>
 		</table>
 
-	</div>
-	<div class="col-sm-12">
-		<?=$pager->links('mnr');?>
-	</div>		
+	</div>			
 </div>
-
-<?php endif; ?>	
